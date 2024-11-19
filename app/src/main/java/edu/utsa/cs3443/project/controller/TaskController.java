a62d636a69bee63a7f28f277917735256af07330
JoeyBarrientes <142752045+JoeyBarrientes@users.noreply.github.com>
2024-11-19 16:44:27 -0600

    Add Task Class Logic

--- app/src/main/java/edu/utsa/cs3443/project/controller/TaskController.java
+++ app/src/main/java/edu/utsa/cs3443/project/controller/TaskController.java
@@ -2,5 +2,5 @@ package edu.utsa.cs3443.project.controller;
 
 import edu.utsa.cs3443.project.model.Task;
 
-public class TaskController extends Task {
+public class TaskController {
 }
--- app/src/main/java/edu/utsa/cs3443/project/model/Task.java
+++ app/src/main/java/edu/utsa/cs3443/project/model/Task.java
@@ -1,4 +1,37 @@
 package edu.utsa.cs3443.project.model;
 
 public class Task {
+    private String date;
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String date, String description, boolean isCompleted){
+        this.date = date;
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean getCompleted(){
+        return isCompleted;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setCompleted(boolean isCompleted){
+        this.isCompleted = isCompleted;
+    }
 }
