56b46774661a6025b5db159d89d04ffa293c881b
JoeyBarrientes <142752045+JoeyBarrientes@users.noreply.github.com>
2024-11-19 16:23:02 -0600

    Add MVC FIles

--- app/src/main/java/edu/utsa/cs3443/project/TaskCreationActivity.java
+++ app/src/main/java/edu/utsa/cs3443/project/TaskCreationActivity.java
@@ -1,4 +1,4 @@
-package edu.utsa.cs3443.project.model;
+package edu.utsa.cs3443.project;
 
 import android.os.Bundle;
 
@@ -8,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import edu.utsa.cs3443.project.R;
-
-public class CreateTask extends AppCompatActivity {
+public class TaskCreationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_create_task);
+        setContentView(R.layout.activity_task_creation);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/edu/utsa/cs3443/project/controller/TaskController.java
@@ -0,0 +1,6 @@
+package edu.utsa.cs3443.project.controller;
+
+import edu.utsa.cs3443.project.model.Task;
+
+public class TaskController extends Task {
+}
