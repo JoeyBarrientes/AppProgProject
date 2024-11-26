26781c3f1242523ab0fa4b0038747163298329d1
JoeyBarrientes <142752045+JoeyBarrientes@users.noreply.github.com>
2024-11-26 16:39:05 -0600

    Work on Homepage UI and Controller
    -Added homepage button icons
    -Implemented on click screen change functionality to buttons

--- app/src/main/java/edu/utsa/cs3443/project/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/project/MainActivity.java
@@ -1,12 +1,15 @@
 package edu.utsa.cs3443.project;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import edu.utsa.cs3443.project.controller.HomepageController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button homeBtn = findViewById(R.id.homeButton);
+        Button overviewBtn = findViewById(R.id.overviewButton);
+        Button progressBtn = findViewById(R.id.progressButton);
+        Button createBtn = findViewById(R.id.createButton);
+        // Create MainController and set it as the click listener for all buttons
+        // as to let the controller perform necessary input handling
+        HomepageController homepageController = new HomepageController(this);
+        homeBtn.setOnClickListener(homepageController);
+        overviewBtn.setOnClickListener(homepageController);
+        progressBtn.setOnClickListener(homepageController);
+        createBtn.setOnClickListener(homepageController);
     }
 }
